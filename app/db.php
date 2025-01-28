ffef828a6c2919985fce364e85a91711aa601262
Tachera Sasi <tacherasasi@gmail.com>
2025-01-28 11:16:46 +0300

    db::connect

--- app/db.php
+++ app/db.php
@@ -3,6 +3,11 @@ class DB{
     public static ?PDO $conn = null;
 
     public static function connect(string $dsn, string $username = '', string $password = '', array $options = []){
-
+        try {
+            self::$conn = new PDO($dsn, $username, $password, $options);
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Error while connect to the db ".$e->getMessage());
+        }
     }
 }
