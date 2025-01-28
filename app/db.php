f255c28712a38d137e5f15c73fb8e9f3840038c7
Tachera Sasi <tacherasasi@gmail.com>
2025-01-28 11:36:40 +0300

    more db::methods

--- app/db.php
+++ app/db.php
@@ -7,13 +7,13 @@ class DB{
             self::$conn = new PDO($dsn, $username, $password, $options);
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die("Error while connect to the db ".$e->getMessage());
+            die("Error while connecting to the db ".$e->getMessage());
         }
     }
 
     public static function query(string $sql, array $params = []): bool|PDOStatement {
         if (!self::$conn) {
-            throw new Exception("Database not connected. Call Database::connect() first.");
+            throw new Exception("DB not connected. Call DB::connect() first.");
         }
 
         try {
@@ -23,6 +23,23 @@ class DB{
         } catch (PDOException $e) {
             die("Query failed: " . $e->getMessage());
         }
+    }
+
+    public static function fetchAll(string $sql, array $params = []): array {
+        $stmt = self::query($sql, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    
+    public static function fetchOne(string $sql, array $params = []): array|false {
+        $stmt = self::query($sql, $params);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    
+    public static function fetchObjects(string $sql, array $params = [], string $className): array {
+        $stmt = self::query($sql, $params);
+        return $stmt->fetchAll(PDO::FETCH_CLASS, $className);
     }
 
 }
