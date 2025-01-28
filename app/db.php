f990e493058c4dee3e6ee8f0a9bb953e4861966c
Tachera Sasi <tacherasasi@gmail.com>
2025-01-28 11:13:25 +0300

    web assignment init

---
+++ app/db.php
@@ -0,0 +1,8 @@
+<?php 
+class DB{
+    public static ?PDO $conn = null;
+
+    public static function connect(string $dsn, string $username = '', string $password = '', array $options = []){
+
+    }
+}
