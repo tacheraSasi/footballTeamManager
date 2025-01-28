bbd709f4219f7f1047d18b9283be04318c2dfcfe
Tachera Sasi <tacherasasi@gmail.com>
2025-01-28 19:03:56 +0300

    add login handler with email and password validation

---
+++ handlers/login.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '../app/auth.php';
+
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
+
+if ($email && $password) {
+    Auth::login($email, $password);
+} else {
+    App::error("Please fill out all fields.");
+    App::redirect("login.php");
+}
